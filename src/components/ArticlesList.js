7b52c84019b5436f4d8f2895ee3e7d976a89da9d
jemish kevadiya <jemishkevadiya007@gmail.com>
2024-11-16 12:23:00 -0500

    All required setUp has been done like NotFoundpage.js and completed frontend.

--- src/components/ArticlesList.js
+++ src/components/ArticlesList.js
@@ -4,11 +4,13 @@ const ArticlesList = ({ articles }) => {
     return (
         <>
         {articles.map(article => (
-            <Link key={article.name} className="article-list-item" to={`/articles/${article.name}`}>
+            <div key={article.name} className="article-list-item" >
+                <Link to={`/articles/${article.name}`}>
                 <h3>{article.title}</h3>
+                </Link>
                 <p>{article.content[0].substring(0, 150)}...</p>
-            </Link>
-        ))}
+            </div>       
+        ))}     
         </>
     );
 }
