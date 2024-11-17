33993a35febfff5c80695e28eaedad51d90ae00e
jemish kevadiya <jemishkevadiya007@gmail.com>
2024-11-16 19:27:45 -0500

    Added axios library and calling useEffect at the right time

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,18 +1,27 @@
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import NotFoundPage from './NotFoundPage';
 import articles from './article-content'; 
 
 const ArticlePage = () => {
+
+    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] })
+
+    useEffect(() => {
+        setArticleInfo({ upvotes: Math.ceil(Math.random() * 10), comments: [] })
+    }, []);
+
     const { articleId } = useParams();
     const article = articles.find(article => article.name === articleId);
 
     if (!article) {
         return <NotFoundPage />
-    }
+    } 
     
     return (
         <>
-        <h1>{article.title}</h1>
+        <h1>{article.title}</h1> 
+        <p>This article has {articleInfo.upvotes} upvote(s)</p>
         {article.content.map((paragraph, i) => (
             <p key={i}>{paragraph}</p>
         ))}
